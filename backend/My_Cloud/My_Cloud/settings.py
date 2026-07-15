b040d5bd0fd1a20313dd6de33983aa1e2e4eb378
Nickolay <tereshcuk@yandex.ru>
2026-07-15 21:36:41 +0300

    diplom 15/07/2026 21:36

--- backend/My_Cloud/My_Cloud/settings.py
+++ backend/My_Cloud/My_Cloud/settings.py
@@ -142,7 +142,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
+STATICFILES_DIRS = [
+    BASE_DIR / "static", # Если у вас есть личная папка static в корне проекта
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
